92f57dbc7694c05ef6e2a599e712335189efd81b
cuongng22 <theendvg2003@gmail.com>
2022-05-20 09:02:43 +0700

    add

---
+++ 2. TypeScript/MD5-LS2-Practice4/src/main.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var rectangle_1 = require("./rectangle");
+var rectangle = new rectangle_1.Rectangle(5, 6);
+console.log(rectangle.area());
+console.log(rectangle.toString());
---
+++ 2. TypeScript/MD5-LS2-Practice4/src/main.ts
@@ -0,0 +1,5 @@
+import {Rectangle} from "./rectangle";
+
+const rectangle = new Rectangle(5 ,6);
+console.log(rectangle.area());
+console.log(rectangle.toString());
